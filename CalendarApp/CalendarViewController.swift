4a2adea94f5ac35d636d71529f8143dec2723262
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-07-07 11:13:04 +0300

    events from Calendar

--- CalendarApp/CalendarViewController.swift
+++ CalendarApp/CalendarViewController.swift
@@ -7,15 +7,48 @@
 
 import UIKit
 import CalendarKit
+import EventKit
 
 class ViewController: DayViewController {
+    
+    private let eventStore = EKEventStore()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Calendar"
+        requestAccessToCakendar()
     }
 
-
+    func requestAccessToCakendar() {
+        eventStore.requestAccess(to: .event) { success, error in
+            
+        }
+    }
+    
+    override func eventsForDate(_ date: Date) -> [EventDescriptor] {
+        let startDate = date
+        var oneDayComponents = DateComponents()
+        oneDayComponents.day = 1
+        
+        let endDate = calendar.date(byAdding: oneDayComponents, to: startDate)!
+        
+        let predicate = eventStore.predicateForEvents(withStart: startDate, end: endDate, calendars: nil)
+        
+        let eventKitEvents = eventStore.events(matching: predicate)
+        
+        let calendarKitEvents = eventKitEvents.map { ekEvent -> Event in
+            let ckEvent = Event()
+            ckEvent.startDate = ekEvent.startDate
+            ckEvent.endDate = ekEvent.endDate
+            ckEvent.isAllDay = ekEvent.isAllDay
+            ckEvent.text = ekEvent.title
+            if let eventColor = ekEvent.calendar.cgColor {
+                ckEvent.color = UIColor(cgColor: eventColor)
+            }
+            return ckEvent
+        }
+        return calendarKitEvents
+    }
 }
 
